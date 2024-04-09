d81c076687c163f044cb7551c6b597a590567b65
Iyanu Oluwa <iyanu1396@gmail.com>
2024-04-09 18:22:10 +0100

    ......

--- components/DropDownInput.jsx
+++ components/DropDownInput.jsx
@@ -1,5 +1,6 @@
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
+import searchIcon from "../src/assets/searchIcon.png";
 
 function DropDownInput() {
   const optionTypes = ["All Type"];
@@ -7,18 +8,24 @@ function DropDownInput() {
 
   const defaultOptionType = optionTypes[0];
   const defaultOptionNeighborhood = optionNeighborhood[0];
+
   return (
-    <div>
+    <div className="header-inputs">
       <Dropdown
+        className="type"
         options={optionTypes}
         value={defaultOptionType}
         placeholder="Select an option"
       />
       <Dropdown
+        className="neighbour"
         options={optionNeighborhood}
         value={defaultOptionNeighborhood}
         placeholder="Select an option"
       />
+      <div className="search-icon-container">
+        <img src={searchIcon} alt="search icon" />
+      </div>
     </div>
   );
 }
